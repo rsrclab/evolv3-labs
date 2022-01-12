b87df5225bb51f5779386a7160cc7bda1c45721a
Tymur Renhach <tymurrenhach@gmail.com>
2022-01-12 20:00:16 +0200

    desktop version done

--- js/script.js
+++ js/script.js
@@ -4,6 +4,39 @@
 
 (function ($) {
   $(document).ready(function () {
+
+    $('.hamburger').on('click', function () {
+      $(this).toggleClass('open');
+      $('.header-menu').toggleClass('open');
+    });
+
+    $('.footer-menu__trigger').on('click', function () {
+      $('.footer-menu').toggleClass('open');
+    });
+
+    var myFullPage = $('#fullpage').fullpage({
+      anchors: ['intro', 'portfolio', 'life', 'mission', 'sev', 'fund', 'team'],
+      menu: '#menu',
+      onLeave: function(origin, destination, direction){
+        var leavingSection = this;
+
+        // Handling Menu and Logo positions
+        if(origin.index == 0 && direction =='down'){
+          $('.header-nav').fadeIn();
+
+        }
+        else if(origin.index == 1 && direction == 'up'){
+          $('.header-nav').fadeOut();
+        }
+      }
+    });
     
+    $('.section.slider .slider-holder').slick({
+      slidesToScroll: 1,
+      infinite: false,
+      arrows: false,
+      variableWidth: true
+    });
+
   });
 })(jQuery);
